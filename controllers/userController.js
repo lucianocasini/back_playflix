e4d5c7a7a7344f1de1647fc3388a71f5d92952e7
lucianocasini <lucianoo.casini@gmail.com>
2022-12-21 17:14:06 -0300

    Add user services

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,100 +1,112 @@
-const { User, UserFavorites } = require("../models");
-const { generateToken } = require("../config/auth");
-const { Op } = require("sequelize");
+const { generateToken } = require('../config/auth');
+const {
+  getUsers,
+  createUser,
+  loginUser,
+  addFavorite,
+  removeFavorite,
+  getFavorites,
+} = require('../services/user');
+const { errorTemplate } = require('../utils/validate');
 
-exports.getUsers = (req, res, next) => {
-  User.findAll()
-    .then((users) => res.send(users))
-    .catch(next);
+exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await getUsers();
+    res.send(users);
+  } catch (e) {
+    next(e);
+  }
 };
 
 exports.createUser = async (req, res, next) => {
-  const userExists = await User.findOne({
-    where: {
-      [Op.or]: [{ username: req.body.username }, { email: req.body.email }],
-    },
-  });
-  if (userExists)
-    return res.status(409).send("El usuario o email ya se encuentra en uso");
-
-  User.create(req.body)
-    .then((user) => res.status(201).send(user))
-    .catch(next);
-};
-
-exports.addFavorite = async (req, res, next) => {
-  const { id, type, title, poster } = req.body;
-  const user = req.user;
-  const favoriteExists = await UserFavorites.findOne({
-    where: { contentId: id, contentType: type, userId: user.id },
-  }).catch(next);
-  if (favoriteExists)
-    return res.status(409).send("Este contenido ya fue añadido a favoritos");
-
-  UserFavorites.create({
-    title,
-    poster,
-    contentId: id,
-    contentType: type,
-    userId: user.id,
-  })
-    .then(({ id, title, poster, contentId, contentType }) => {
-      const favItem = { id, title, poster, contentId, contentType };
-      res.status(201).send(favItem);
-    })
-    .catch(next);
-};
-
-exports.removeFavorite = (req, res, next) => {
-  const { id } = req.params;
-  const user = req.user;
-  UserFavorites.destroy({ where: { id, userId: user.id } })
-    .then(() => res.sendStatus(204))
-    .catch(next);
-};
-
-exports.getFavorites = (req, res, next) => {
-  const { type } = req.query;
-  const user = req.user;
-  let where = { userId: user.id };
-  if (type) where = { ...where, contentType: type };
-  UserFavorites.findAll({
-    where,
-    attributes: ["id", "contentId", "contentType", "title", "poster"],
-  })
-    .then((favs) => res.send(favs))
-    .catch(next);
+  try {
+    const userData = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const userCreated = await createUser(userData);
+    res.status(201).send(userCreated);
+  } catch (e) {
+    next(e);
+  }
 };
 
 exports.getLoggedUser = (req, res, next) => {
-  res.send(req.user);
+  try {
+    res.send(req.user);
+  } catch (e) {
+    next(e);
+  }
 };
 
-exports.loginUser = (req, res, next) => {
-  const { username, password } = req.body;
+exports.loginUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await loginUser(username, password);
 
-  User.findOne({ where: { username } })
-    .then((user) => {
-      if (!user)
-        return res.status(400).send("Usuario o contraseña incorrectos");
+    if (!user)
+      return res
+        .status(400)
+        .send(errorTemplate('Usuario o contraseña incorrectos'));
 
-      user.validatePassword(password).then((isValid) => {
-        if (!isValid)
-          return res.status(400).send("Usuario o contraseña incorrectos");
-        const userData = {
-          id: user.id,
-          username: user.username,
-          email: user.email,
-        };
-        const token = generateToken(userData);
-        res.cookie("token", token);
-        res.json(userData);
-      });
-    })
-    .catch(next);
+    const userData = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = generateToken(userData);
+    res.cookie('token', token);
+    res.json(userData);
+  } catch (e) {
+    next(e);
+  }
 };
 
 exports.logoutUser = (req, res, next) => {
-  res.clearCookie("token");
+  res.clearCookie('token');
   res.sendStatus(200);
+};
+
+exports.addFavorite = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    const favoriteData = {
+      title: req.body.title,
+      poster: req.body.poster,
+      contentId: req.body.id,
+      contentType: req.body.type,
+      userId: user.id,
+    };
+
+    const favorite = await addFavorite(favoriteData);
+    const { id, title, poster, contentId, contentType } = favorite;
+    res.status(201).send({ id, title, poster, contentId, contentType });
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.removeFavorite = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user.id;
+    await removeFavorite(id, userId);
+    res.sendStatus(204);
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.getFavorites = async (req, res, next) => {
+  try {
+    const { type } = req.query;
+    const userId = req.user.id;
+    const favorites = await getFavorites(userId, type);
+    res.send(favorites);
+  } catch (e) {
+    next(e);
+  }
 };
