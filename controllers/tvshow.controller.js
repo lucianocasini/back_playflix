5f01e72893dae37be4a65ec5bf031861090e7d80
lucianocasini <lucianoo.casini@gmail.com>
2022-12-28 23:46:50 -0300

    Add TV show services

--- controllers/tvshow.controller.js
+++ controllers/tvshow.controller.js
@@ -1,32 +1,20 @@
-const axios = require("axios");
-const { API_URL, API_KEY } = require("../config/api");
-const { imageUrlGenerator } = require("../utils/images");
-const { isNumeric } = require("../utils/validations");
+const { getTvShow, getTvShowDetails } = require('../services/tvshow.service');
 
-exports.getTvShow = (req, res, next) => {
-  const url = `${API_URL}/discover/tv?sort_by=popularity.desc&language=es-ES&api_key=${API_KEY}`;
-  axios
-    .get(url)
-    .then((res) => res.data)
-    .then((tv) => {
-      tv.results = imageUrlGenerator(tv.results);
-      res.send(tv);
-    })
-    .catch(next);
+exports.getTvShow = async (req, res, next) => {
+  try {
+    const tvshow = await getTvShow();
+    res.send(tvshow);
+  } catch (e) {
+    next(e);
+  }
 };
 
-exports.getTvShowDetails = (req, res, next) => {
-  const { id } = req.params;
-  if (!isNumeric(id)) return res.sendStatus(400);
-  const url = `${API_URL}/tv/${id}?language=es-ES&api_key=${API_KEY}`;
-  axios
-    .get(url)
-    .then((res) => res.data)
-    .then((tvshow) => {
-      tvshow = imageUrlGenerator([tvshow])[0];
-      tvshow.title = tvshow.name;
-      tvshow.release_date = tvshow.first_air_date;
-      res.send(tvshow);
-    })
-    .catch(next);
+exports.getTvShowDetails = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const tvshowDetails = await getTvShowDetails(id);
+    res.send(tvshowDetails);
+  } catch (e) {
+    next(e);
+  }
 };
